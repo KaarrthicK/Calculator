d74a829769564dafea7ec107a62d4ff60ae0cf93
Karthick S <136822998+KaarrthicK@users.noreply.github.com>
2024-02-08 13:07:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let output=document.getElementById("output");
+    function display(num) {
+        output.value +=num;
+    }
+    function Calculate() {
+        try{
+            output.value=eval(output.value);
+        } catch(Error) {
+            alert("Invalid")
+        }
+    }
+    function Clear() {
+        output.value = "";
+    }
+    function del() {
+        output.value = output.value.slice(0,-1);
+    }
